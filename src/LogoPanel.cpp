5212a78bfccce30d9fe4280e203365eff69e34fb
GitJo0n <lee2prettykr@naver.com>
2025-05-18 19:05:10 +0900

    LogoPanel 클래스에 QFrame을 추가하고 레이아웃을 개선하여 콘텐츠 표시 방식을 개선함

--- src/LogoPanel.cpp
+++ src/LogoPanel.cpp
@@ -1,27 +1,52 @@
-#include <rviz/panel.h>
+#include <rviz/panel.h> // ROS 1 rviz
 #include <QLabel>
 #include <QVBoxLayout>
 #include <QPixmap>
+#include <QFrame> // QFrame 추가
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
 namespace my_rviz_panel
 {
-  class LogoPanel : public rviz::Panel
+  class LogoPanel : public rviz::Panel 
   {
     Q_OBJECT
   public:
     LogoPanel(QWidget* parent = 0)
-      : rviz::Panel(parent)
+      : rviz::Panel(parent) 
     {
-      label_ = new QLabel(this);
+      // 콘텐츠를 담을 QFrame 생성
+      QFrame* content_frame = new QFrame(this);
+      content_frame->setObjectName("contentFrame"); // QSS에서 선택자로 사용하기 좋음
+      content_frame->setStyleSheet(
+        "QFrame#contentFrame {"
+        "  border: 3px solid purple;"    // 3픽셀 보라색 실선 테두리
+        "  border-radius: 8px;"         // 모서리 둥글게
+        "  background-color: white;"     // 프레임 배경색
+        "  padding: 10px;"               // 프레임 내부 여백
+        "}"
+      );
+
+      // QLabel은 content_frame의 자식으로 생성
+      label_ = new QLabel(content_frame);
       QPixmap pixmap("/home/user/catkin_ws/src/my_rviz_panel/resources/logo.png");
-      label_->setPixmap(pixmap);
+      if(pixmap.isNull()){
+        ROS_WARN("LogoPanel: Failed to load logo.png. Check path.");
+        label_->setText("Logo not found");
+      } else {
+        label_->setPixmap(pixmap);
+      }
       label_->setAlignment(Qt::AlignCenter);
 
-      QVBoxLayout* layout = new QVBoxLayout;
-      layout->addWidget(label_);
-      setLayout(layout);
+      // QFrame 내부 레이아웃 설정
+      QVBoxLayout* frame_layout = new QVBoxLayout(content_frame);
+      frame_layout->addWidget(label_);
+      // content_frame->setLayout(frame_layout); // 생성자에 부모 위젯을 전달하면 자동 설정됨
+
+      // 패널의 메인 레이아웃 설정 (QFrame을 패널에 추가)
+      QVBoxLayout* panel_layout = new QVBoxLayout(this);
+      panel_layout->addWidget(content_frame);
+      setLayout(panel_layout);
 
       ros::NodeHandle nh;
       subscriber_ = nh.subscribe("/status_text", 10, &LogoPanel::updateText, this);
@@ -40,5 +65,5 @@ namespace my_rviz_panel
 }
 
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(my_rviz_panel::LogoPanel, rviz::Panel)
+PLUGINLIB_EXPORT_CLASS(my_rviz_panel::LogoPanel, rviz::Panel) // 또는 rviz::Panel
 #include "LogoPanel.moc"
