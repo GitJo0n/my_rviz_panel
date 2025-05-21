ab8f04a928989fa7ad3efacadf2a4b25cdd997bc
GitJo0n <lee2prettykr@naver.com>
2025-05-21 14:24:00 +0900

    Control_Keyboard 클래스의 키 이벤트 처리 로직을 개선하고 cmd_vel 퍼블리셔를 추가했습니다. 방향 이미지 업데이트 로직을 수정하여 키 입력에 따라 적절한 이미지를 표시하도록 변경했습니다. 불필요한 코드 주석을 제거하고 코드 가독성을 향상시켰습니다.

--- src/Control_Keyboard.cpp
+++ src/Control_Keyboard.cpp
@@ -9,6 +9,7 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <ros/package.h>
+#include <geometry_msgs/Twist.h>
 #include <pluginlib/class_list_macros.h>
 
 namespace my_rviz_panel
@@ -16,113 +17,168 @@ namespace my_rviz_panel
 
 class Control_Keyboard : public rviz::Panel
 {
-Q_OBJECT
-public:
-  Control_Keyboard(QWidget* parent = 0)
-    : rviz::Panel(parent)
-  {
-    pkg_path_ = ros::package::getPath("my_rviz_panel");
-    if (pkg_path_.empty()) {
-      ROS_ERROR("Control_Keyboard: Could not find package path.");
-    }
+  Q_OBJECT
+  public:
+    Control_Keyboard(QWidget* parent = 0)
+      : rviz::Panel(parent)
+    {
+      pkg_path_ = ros::package::getPath("my_rviz_panel");
+      if (pkg_path_.empty()) {
+        ROS_ERROR("Control_Keyboard: Could not find package path.");
+      }
 
-    // 포커스를 받도록 설정
-    setFocusPolicy(Qt::StrongFocus);
-    setFocus();
+      // 포커스를 받도록 설정
+      setFocusPolicy(Qt::StrongFocus);
+      setFocus();
 
-    QFrame* content_frame = new QFrame(this);
-    content_frame->setObjectName("keyboardContentFrame");
-    content_frame->setStyleSheet(
-      "QFrame#keyboardContentFrame {"
-      "  border: 4px solid orange;"
-      "  border-radius: 8px;"
-      "  background-color: white;"
-      "  padding: 10px;"
-      "}"
-    );
+      QFrame* content_frame = new QFrame(this);
+      content_frame->setObjectName("keyboardContentFrame");
+      content_frame->setStyleSheet(
+        "QFrame#keyboardContentFrame {"
+        "  border: 4px solid orange;"
+        "  border-radius: 8px;"
+        "  background-color: white;"
+        "  padding: 10px;"
+        "}"
+      );
 
-    direction_label_ = new QLabel(content_frame);
-    direction_label_->setAlignment(Qt::AlignCenter);
-    direction_label_->setScaledContents(true);
-    direction_label_->setFixedSize(400, 200);
+      cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
-    QVBoxLayout* frame_layout = new QVBoxLayout(content_frame);
-    frame_layout->addWidget(direction_label_);
+      direction_label_ = new QLabel(content_frame);
+      direction_label_->setAlignment(Qt::AlignCenter);
+      direction_label_->setScaledContents(true);
+      direction_label_->setFixedSize(400, 200);
 
-    QVBoxLayout* panel_layout = new QVBoxLayout(this);
-    panel_layout->addWidget(content_frame);
-    setLayout(panel_layout);
+      QVBoxLayout* frame_layout = new QVBoxLayout(content_frame);
+      frame_layout->addWidget(direction_label_);
 
-    idle_timer_ = new QTimer(this);
-    idle_timer_->setSingleShot(true);
-    connect(idle_timer_, &QTimer::timeout, this, &Control_Keyboard::showIdleImage);
+      cmd_vel.linear.x = 0.0;
+      cmd_vel.angular.z = 0.0;
+      QVBoxLayout* panel_layout = new QVBoxLayout(this);
+      panel_layout->addWidget(content_frame);
+      setLayout(panel_layout);
 
-    showIdleImage();
-  }
+      idle_timer_ = new QTimer(this);
+      idle_timer_->setSingleShot(true);
+      connect(idle_timer_, &QTimer::timeout, this, &Control_Keyboard::showIdleImage);
 
-protected:
-  void keyPressEvent(QKeyEvent* event) override
-  {
-    idle_timer_->stop();
-
-    QString key_text = event->text().toLower();
-    std::string image_file_suffix;
-    bool is_known_key = true;
-
-    if (key_text == "w") image_file_suffix = "/resources/forward.png";
-    else if (key_text == "s") image_file_suffix = "/resources/backward.png";
-    else if (key_text == "a") image_file_suffix = "/resources/left.png";
-    else if (key_text == "d") image_file_suffix = "/resources/right.png";
-    else is_known_key = false;
-
-    if (pkg_path_.empty()) {
-      ROS_ERROR_THROTTLE(5.0, "Package path not set");
-      direction_label_->setText("Error: pkg_path not set");
-      return;
-    }
-
-    if (!is_known_key) {
       showIdleImage();
-      return;
     }
 
-    std::string full_path = pkg_path_ + image_file_suffix;
-    QPixmap pixmap(QString::fromStdString(full_path));
+  protected:
+    void keyPressEvent(QKeyEvent* event) override
+    {
+      QString key_text = event->text().toLower();
 
-    if (pixmap.isNull()) {
-      ROS_WARN_THROTTLE(2.0, "Failed to load image: %s", full_path.c_str());
-      direction_label_->setText(QString("Image not found:\n%1").arg(QString::fromStdString(image_file_suffix).split('/').last()));
-    } else {
-      direction_label_->setPixmap(pixmap);
+      if (key_text == "w") {
+        cmd_vel.linear.x = 1.0;
+      } else if (key_text == "s") {
+        cmd_vel.linear.x = 0.0;
+        cmd_vel.angular.z = 0.0;
+      } else if (key_text == "a") {
+        cmd_vel.angular.z = 0.6;
+      } else if (key_text == "d") {
+        cmd_vel.angular.z = -0.6;
+      } else {
+        return;
+      }
+      cmd_vel_pub_.publish(cmd_vel);
+      updateDirectionImageFromTwist(cmd_vel);
+      // idle_timer_->stop();
+
+      // QString key_text = event->text().toLower();
+      // std::string image_file_suffix;
+      // bool is_known_key = true;
+
+      // if (key_text == "w") image_file_suffix = "/resources/forward.png";
+      // else if (key_text == "s") image_file_suffix = "/resources/backward.png";
+      // else if (key_text == "a") image_file_suffix = "/resources/left.png";
+      // else if (key_text == "d") image_file_suffix = "/resources/right.png";
+      // else is_known_key = false;
+
+      // if (pkg_path_.empty()) {
+      //   ROS_ERROR_THROTTLE(5.0, "Package path not set");
+      //   direction_label_->setText("Error: pkg_path not set");
+      //   return;
+      // }
+
+      // if (!is_known_key) {
+      //   showIdleImage();
+      //   return;
+      // }
+
+      // std::string full_path = pkg_path_ + image_file_suffix;
+      // QPixmap pixmap(QString::fromStdString(full_path));
+
+      // if (pixmap.isNull()) {
+      //   ROS_WARN_THROTTLE(2.0, "Failed to load image: %s", full_path.c_str());
+      //   direction_label_->setText(QString("Image not found:\n%1").arg(QString::fromStdString(image_file_suffix).split('/').last()));
+      // } else {
+      //   direction_label_->setPixmap(pixmap);
+      // }
+
+      // idle_timer_->start(200);  // 일정 시간 뒤 idle 이미지로 복귀
+    }
+    void updateDirectionImageFromTwist(const geometry_msgs::Twist& cmd_vel)
+    {
+      std::string image_path;
+
+      if (cmd_vel.linear.x > 0) {
+        if (cmd_vel.angular.z > 0) {
+          image_path = "/resources/forward_left.png";
+        } else if (cmd_vel.angular.z < 0) {
+          image_path = "/resources/forward_right.png";
+        } else {
+          image_path = "/resources/forward.png";
+        } 
+      } else {
+        if (cmd_vel.angular.z > 0) {
+          image_path = "/resources/right.png";
+        } else if (cmd_vel.angular.z < 0) {
+          image_path = "/resources/left.png";
+        } else {
+          showIdleImage();
+          return;
+        }
+      }
+
+      std::string full_path = pkg_path_ + image_path;
+      QPixmap pixmap(QString::fromStdString(full_path));
+      if (!pixmap.isNull()) {
+        direction_label_->setPixmap(pixmap.scaled(direction_label_->size(), Qt::KeepAspectRatio));
+      } else {
+        direction_label_->setText("Image not found");
+      }
     }
 
-    idle_timer_->start(200);  // 일정 시간 뒤 idle 이미지로 복귀
-  }
+    ros::Publisher cmd_vel_pub_;
+    ros::NodeHandle nh_;
 
-private Q_SLOTS:
-  void showIdleImage()
-  {
-    if (pkg_path_.empty()) {
-      direction_label_->setText("Error: pkg_path not set");
-      return;
+  private Q_SLOTS:
+    void showIdleImage()
+    {
+      if (pkg_path_.empty()) {
+        direction_label_->setText("Error: pkg_path not set");
+        return;
+      }
+
+      std::string idle_image_path = pkg_path_ + "/resources/idle.png";
+      QPixmap pixmap(QString::fromStdString(idle_image_path));
+
+      if (pixmap.isNull()) {
+        ROS_WARN("Idle image not found: %s", idle_image_path.c_str());
+        direction_label_->setText("idle.png not found");
+      } else {
+        direction_label_->setPixmap(pixmap);
+      }
     }
 
-    std::string idle_image_path = pkg_path_ + "/resources/idle.png";
-    QPixmap pixmap(QString::fromStdString(idle_image_path));
-
-    if (pixmap.isNull()) {
-      ROS_WARN("Idle image not found: %s", idle_image_path.c_str());
-      direction_label_->setText("idle.png not found");
-    } else {
-      direction_label_->setPixmap(pixmap);
-    }
-  }
-
-private:
-  QLabel* direction_label_;
-  std::string pkg_path_;
-  QTimer* idle_timer_;
-};
+  private:
+    QLabel* direction_label_;
+    std::string pkg_path_;
+    QTimer* idle_timer_;
+    geometry_msgs::Twist cmd_vel;
+  };
 
 }  // namespace my_rviz_panel
 
