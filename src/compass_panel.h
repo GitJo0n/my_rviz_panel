a967189bb2ee770959fefe82a855097a8985f341
GitJo0n <lee2prettykr@naver.com>
2025-09-08 22:11:26 +0900

    private 섹션에서 angle_label_와 degreesToCardinalString 함수 사이에 공백 추가 및 코드 정리

--- src/compass_panel.h
+++ src/compass_panel.h
@@ -17,11 +17,13 @@ protected Q_SLOTS:
     void onUpdate(); // 주기적으로 호출될 함수
 
 private:
-    QLabel* angle_label_; // 현재 각도를 표시할 라벨
+    ::QLabel* angle_label_; // 현재 각도를 표시할 라벨
+
     CompassWidget* compass_widget_;
     QTimer* timer_;
-    QString degreesToCardinalString(double degrees); // 각도를 방위각 문자열로 변환하는 함수
+    
+    QString degreesToCardinalString(double degrees);
 };
-} // end namespace my_compass_panel
+} 
 
 #endif // COMPASS_PANEL_H
